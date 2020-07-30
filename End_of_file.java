dcb1e4f888cf1ba6717eb3387ee363a3f6fe9a8b
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-07-30 19:13:48 +0530

    Create End_of_file.java

---
+++ End_of_file.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s; int c = 1;
+        while(sc.hasNextLine()){
+             s = sc.nextLine();
+            
+            System.out.println(c  +" "+s);
+            c++;
+        }
+    }
+}
