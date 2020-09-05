1a29d116083d5b4ab51bda5ad5ef11e6132d2833
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-09-05 19:58:51 +0530

    Create Sparse_Arrays.java

---
+++ Array/Sparse_Arrays.java
@@ -0,0 +1,228 @@
+/*There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings.
+
+For example, given input  and , we find  instances of ',  of '' and  of ''. For each query, we add an element to our return array, .
+
+Function Description
+
+Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+
+matchingStrings has the following parameters:
+
+strings - an array of strings to search
+queries - an array of query strings
+Input Format
+
+The first line contains and integer , the size of .
+Each of the next  lines contains a string .
+The next line contains , the size of .
+Each of the next  lines contains a string .
+
+Constraints
+
+
+
+ .
+
+Output Format
+
+Return an integer array of the results of all queries in order.
+
+Sample Input 1
+
+CopyDownload
+Array: strings
+aba
+baba
+aba
+xzxb
+
+ 
+
+
+
+Array: queries
+aba
+xzxb
+ab
+
+ 
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+Sample Output 1
+
+2
+1
+0
+Explanation 1
+
+Here, "aba" occurs twice, in the first and third string. The string "xzxb" occurs once in the fourth string, and "ab" does not occur at all.
+
+
+Sample Input 2
+
+CopyDownload
+Array: strings
+def
+de
+fgh
+
+ 
+
+
+
+Array: queries
+de
+lmn
+fgh
+
+ 
+3
+def
+de
+fgh
+3
+de
+lmn
+fgh
+Sample Output 2
+
+1
+0
+1
+
+Sample Input 3
+
+CopyDownload
+Array: strings
+abcde
+sdaklfj
+asdjf
+na
+basdn
+sdaklfj
+asdjf
+na
+asdjf
+na
+basdn
+sdaklfj
+asdjf
+
+ 
+
+
+
+Array: queries
+abcde
+sdaklfj
+asdjf
+na
+basdn
+
+ 
+13
+abcde
+sdaklfj
+asdjf
+na
+basdn
+sdaklfj
+asdjf
+na
+asdjf
+na
+basdn
+sdaklfj
+asdjf
+5
+abcde
+sdaklfj
+asdjf
+na
+basdn
+Sample Output 3
+
+1
+3
+4
+3
+2*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        int i,j,x=0;
+        int count[] = new int[queries.length];
+        for(i=0;i<queries.length;i++){
+            int c=0;
+            for(j=0;j<strings.length;j++){
+                if( queries[i].equals(strings[j]) == true ){
+                    c++;
+                }
+            }
+            count[x] = c;
+            x++;
+        }
+        return count;
+    }
+    
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
